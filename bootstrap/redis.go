29cbfe49a5976f6964faca7fe8e21e4ec8f5b223
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-01-07 14:09:47 +0800

    feat: Using Redis

---
+++ bootstrap/redis.go
@@ -0,0 +1,20 @@
+package bootstrap
+
+import (
+	"GoHub-Service/pkg/config"
+	"GoHub-Service/pkg/redis"
+
+	"fmt"
+)
+
+// SetupRedis 初始化 Redis
+func SetupRedis() {
+
+	// 建立 Redis 连接
+	redis.ConnectRedis(
+		fmt.Sprintf("%v:%v", config.GetString("redis.host"), config.GetString("redis.port")),
+		config.GetString("redis.username"),
+		config.GetString("redis.password"),
+		config.GetInt("redis.database"),
+	)
+}
