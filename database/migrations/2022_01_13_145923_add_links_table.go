5acf3157a1d1f3080aa63352eb698b9910244e80
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-01-13 15:00:30 +0800

    feat: Link model and migration

---
+++ database/migrations/2022_01_13_145923_add_links_table.go
@@ -0,0 +1,31 @@
+package migrations
+
+import (
+	"GoHub-Service/app/models"
+	"GoHub-Service/pkg/migrate"
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2022_01_13_145923_add_links_table", up, down)
+}
