bad98f193850cd293b4697ee6fca57a8abf4d1c0
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-01-13 13:45:03 +0800

    feat: User List Paging

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,6 +1,12 @@
 package user
 
-import "GoHub-Service/pkg/database"
+import (
+	"GoHub-Service/pkg/app"
+	"GoHub-Service/pkg/database"
+	"GoHub-Service/pkg/paginator"
+
+	"github.com/gin-gonic/gin"
+)
 
 // IsEmailExist 判断 Email 已被注册
 func IsEmailExist(email string) bool {
@@ -47,5 +53,17 @@ func GetByEmail(email string) (userModel User) {
 // All 获取所有用户数据
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
