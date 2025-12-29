18b521180da334b8fc1019404251272400474286
Oyxiaoxi <oyxiaoxi@hotmail.com>
2025-12-29 22:19:36 +0800

    refactor: 路由注册支持 api_domain 配置，完善 .env.example，注释优化

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -28,7 +28,7 @@ func registerGlobalMiddleWare(router *gin.Engine) {
     router.Use(
         middlewares.Logger(),
         middlewares.Recovery(),
-        middlewares.ForceUA(),
+        middlewares.ForceUA(),              // 强制请求必须附带 User-Agent 标头
         middlewares.CORS(),                 // CORS 跨域配置
         middlewares.SecureHeaders(),        // 安全响应头
         middlewares.XSSProtection(),        // XSS 防护
--- routes/api.go
+++ routes/api.go
@@ -4,6 +4,7 @@ package routes
 import (
     "GoHub-Service/app/http/controllers/api/v1/auth"
     "GoHub-Service/app/http/middlewares"
+    "GoHub-Service/pkg/config"
 
     controllers "GoHub-Service/app/http/controllers/api/v1"
 
@@ -13,7 +14,15 @@ import (
 // RegisterAPIRoutes 注册 API 相关路由
 
 func RegisterAPIRoutes(r *gin.Engine) {
-    v1 := r.Group("/v1")
+
+    // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+    var v1 *gin.RouterGroup
+    if len(config.Get("app.api_domain")) == 0 {
+        v1 = r.Group("/api/v1")
+    } else {
+        v1 = r.Group("/v1")
+    }
+
     v1.Use(
         middlewares.RequestID(),
         middlewares.LimitIP("200-H"),
